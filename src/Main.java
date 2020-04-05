873d005d7cd3c630c45a6a8cbcc1c4f1139a2173
AnaXaX <anas.boudyach@gmail.com>
2020-04-06 00:36:10 +0200

    Try hard

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Personne personne1 = new Personne("BOUDYACH", "Anas", 24); 
+		System.out.println(personne1.toString());
 	}
 
 }
